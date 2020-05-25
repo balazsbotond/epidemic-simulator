b39c8ab5d643d4ca1c1f48b763bf79c1fa28ffc8
Botond Balazs <balazsbotond@infinitect.hu>
2020-05-25 22:39:40 +0200

    .editorconfig

--- countdown.js
+++ countdown.js
@@ -1,23 +1,23 @@
 class Countdown {
-	constructor(from, onFinish) {
-		this.from = from;
-		this.onFinish = onFinish;
-		this.reset();
-	}
+  constructor(from, onFinish) {
+    this.from = from;
+    this.onFinish = onFinish;
+    this.reset();
+  }
 
-	update() {
-		if (this.finished) {
-			return;
-		}
-		this.current--;
-		if (this.current === 0) {
-			this.finished = true;
-			this.onFinish();
-		}
-	}
+  update() {
+    if (this.finished) {
+      return;
+    }
+    this.current--;
+    if (this.current === 0) {
+      this.finished = true;
+      this.onFinish();
+    }
+  }
 
-	reset() {
-		this.current = this.from;
-		this.finished = false;
-	}
+  reset() {
+    this.current = this.from;
+    this.finished = false;
+  }
 }
